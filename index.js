85c6f05dca6ba9daaac94c0977b31d63ceee10e7
lightyearsaway <jeremy@jeremyleu.com>
2017-06-25 14:04:51 -0700

    added debug, now sends all messages in queue

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ module.exports = function blockLevelOneWhispers(dispatch) {
   var name;
   var cid;
   var whisperQueues = {};
+  var debug = true;
+
+  if(debug) {
+    dispatch.hook('C_ASK_INTERACTIVE', 1, (event) => {
+      console.log(event);
+    });
+  }
 
   dispatch.hook('S_LOGIN', 1, (event) => {
     ({name, cid} = event);
@@ -25,19 +32,23 @@ module.exports = function blockLevelOneWhispers(dispatch) {
   dispatch.hook('S_ANSWER_INTERACTIVE', 2, (event) => {
     if(whisperQueues[event.name] && whisperQueues[event.name].length > 0){
       if(event.level > 1) {
-        dispatch.toClient('S_WHISPER', 1, {
-          player: cid,
-          unk1: 0,
-          gm: 0,
-          unk2: 0,
-          author: event.name,
-          recipient: name,
-          message: whisperQueues[event.name].shift()
-        });
+        while(whisperQueues[event.name].length > 0) {
+          dispatch.toClient('S_WHISPER', 1, {
+            player: cid,
+            unk1: 0,
+            gm: 0,
+            unk2: 0,
+            author: event.name,
+            recipient: name,
+            message: whisperQueues[event.name].shift()
+          });
+        }
       }
       else {
-        whisperQueues[event.name].shift();
-        console.log("message from level 1 character " + event.name + " blocked");
+        while(whisperQueues[event.name].length > 0) {
+          whisperQueues[event.name].shift();
+          console.log("message from level 1 character " + event.name + " blocked");
+        }
       }
     }
     /*if(whisperQueues[event.name] && whisperQueues[event.name].length == 0)
